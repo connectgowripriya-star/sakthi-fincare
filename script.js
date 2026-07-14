e4cb0a2da758155700dc76cae833b2cc6f0238ac
connectgowripriya-star <connectgowripriya@gmail.com>
2026-07-14 21:49:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,33 @@ const menuToggle = document.querySelector(".menu-toggle");
 
 const navLinks = document.querySelector(".nav-links");
 
+
+// ================= MOBILE MENU =================
+
+
+// Open / Close Menu
+
 menuToggle.addEventListener("click", () => {
 
     navLinks.classList.toggle("active");
+
+    menuToggle.textContent =
+        navLinks.classList.contains("active")
+            ? "✕"
+            : "☰";
+
+});
+
+// Auto close after clicking a menu item
+
+document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+        menuToggle.textContent = "☰";
+
+    });
 
 });
